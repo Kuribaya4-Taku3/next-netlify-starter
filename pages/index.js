78033608e33aee0de90896048a31515dcb72560e
kuribayashi <t.kuribayashi0711@gmail.com>
2022-02-07 10:47:09 +0900

    開発用ブランチを作成

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app! Test push" />
+        <Header title="Welcome to my app! Brunch Test!" />
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
